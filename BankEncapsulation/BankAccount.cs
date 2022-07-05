b405c899271a468fd9fac7e45bc4204c47a3ad97
Dustin Key <Keydustin90@gmail.com>
2022-07-05 13:38:13 -0500

    done with BankEncapsulation

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class dustinIsAccount
+    {
+        public dustinIsAccount()
+        {
+        }
+
+        private double balanece = 0;
+
+        public void Deposit(double amount)
+        {
+            balanece = amount;
+        }
+
+        public double GetBalance()
+        {
+            return balanece; 
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class MainClass
+    {
+        private static object dustinIsAccount;
+
+        static void Main(string[] args)
+        {
+
+            var dustinIsAccount = new dustinIsAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposite = double.Parse(Console.ReadLine());
+
+            dustinIsAccount.Deposit(amountToDeposite);
+
+            Console.WriteLine($"Thank you! Your balance is now {dustinIsAccount.GetBalance()}");
+        }
+    }
+}
